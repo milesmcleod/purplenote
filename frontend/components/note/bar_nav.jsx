c551f79e103884c0929f0017ec4128ef2838c3a6
Miles McLeod <milesmcleod123@gmail.com>
2017-11-24 01:50:19 -0800

    Implement icons for nav buttons and hover effects

--- frontend/components/note/bar_nav.jsx
+++ frontend/components/note/bar_nav.jsx
@@ -26,22 +26,32 @@ class BarNav extends React.Component {
             onClick={() => {
               this.renderNewNote();
               this.props.enterFullscreen();
-            }}>P</li>
-          <li className="bar-nav-button-search">Q</li>
+            }}><div className="hover-bar-nav-button-new-note"></div></li>
+          <li className="bar-nav-button-search">
+            <div className="hover-bar-nav-button-search"></div>
+          </li>
         </ul>
         <ul className='bar-nav-button-list-2'>
           <li
             className="bar-nav-button-shortcuts"
-            onClick={() => this.handleNavChange('shortcuts')}>S</li>
+            onClick={() => this.handleNavChange('shortcuts')}>
+            <div className="hover-bar-nav-button-shortcuts"></div>
+          </li>
           <li
             className="bar-nav-button-notes"
-            onClick={() => this.handleNavChange('notes')}>N</li>
+            onClick={() => this.handleNavChange('notes')}>
+            <div className="hover-bar-nav-button-notes"></div>
+          </li>
           <li
             className="bar-nav-button-notebooks"
-            onClick={() => this.handleNavChange('notebooks')}>B</li>
+            onClick={() => this.handleNavChange('notebooks')}>
+            <div className="hover-bar-nav-button-notebooks"></div>
+          </li>
           <li
             className="bar-nav-button-tags"
-            onClick={() => this.handleNavChange('tags')}>T</li>
+            onClick={() => this.handleNavChange('tags')}>
+            <div className="hover-bar-nav-button-tags"></div>
+          </li>
         </ul>
         <div className="bar-nav-user-info">User</div>
       </nav>
