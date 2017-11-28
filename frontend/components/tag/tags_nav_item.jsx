33a1b9b65099652b29449a5ac104b0b8ebfed85c
Miles McLeod <milesmcleod123@gmail.com>
2017-11-27 17:15:22 -0800

    Add slide out JS/CSS transitions on all tag nav items

--- frontend/components/tag/tags_nav_item.jsx
+++ frontend/components/tag/tags_nav_item.jsx
@@ -18,6 +18,7 @@ class TagsNavItem extends React.Component {
 
   handleChange(e) {
     e.preventDefault();
+    e.stopPropagation();
     this.setState({
       title: e.target.value
     });
@@ -33,26 +34,68 @@ class TagsNavItem extends React.Component {
     }, 0);
   }
 
+  slideEditTitle() {
+    const tagInfo = document.getElementById(`title-${this.props.tag.id}`);
+    tagInfo.classList.add('tags-nav-item-info-slide');
+  }
+
+  showEditForm() {
+    console.log('hit');
+    window.setTimeout(() => {
+      const tagInfo = document.getElementById(`title-${this.props.tag.id}`);
+      const icons = document.getElementsByClassName(`tag-icon-${this.props.tag.id}`);
+      for (let i = 0; i < icons.length; i ++) {
+        icons[i].classList.add('hide-tag-elements');
+      }
+      const container = document.getElementById(`tags-nav-item-${this.props.tag.id}`);
+      container.classList.add('tags-no-show-on-hover');
+      tagInfo.classList.add('hide-tag-elements');
+      const tagForm = document.getElementById(`form-${this.props.tag.id}`);
+      tagForm.classList.add('edit-tag-form-show');
+      tagForm.focus();
+      const value = tagForm.value;
+      tagForm.value = '';
+      tagForm.value = value;
+    }, 501);
+  }
+
+
   render() {
     return (
-      <div className={
-          (this.props.selected) ? 'tags-nav-item-selected' : 'tags-nav-item'
-        }>
-        <div className='tags-nav-item-info'>
-          <h4>{this.state.title}</h4><p>{this.props.tagCount}</p>
-        </div>
-        <span className='tag-icon-star'>S</span>
-        <span className='tag-icon-edit'>E</span>
-        <span
-          className='tag-icon-trash'
-          onClick={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            this.props.enterTagDeletion(this.props.tag.id);
-            this.showDeleteTagModal();
-          }}
-          >T</span>
+      <div
+        id={`tags-nav-item-${this.props.tag.id}`}
+        className={
+          (this.props.selected) ? 'tags-nav-item tags-nav-item-selected' : 'tags-nav-item'
+        }
+        >
+        <section className='tags-item-container'>
+          <div
+            className='tags-nav-item-info' id={`title-${this.props.tag.id}`}>
+            <h4>{this.state.title}<p>{this.props.tagCount}</p></h4>
+          </div>
+          <span className={`tag-icon-star tag-icon-${this.props.tag.id}`}>S</span>
+          <span
+            className={`tag-icon-edit tag-icon-${this.props.tag.id}`}
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              this.showEditForm();
+              this.slideEditTitle();
+            }}
+            >E</span>
+          <span
+            className={`tag-icon-trash tag-icon-${this.props.tag.id}`}
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              this.props.enterTagDeletion(this.props.tag.id);
+              this.showDeleteTagModal();
+            }}
+            >T</span>
+        </section>
         <input
+          id={`form-${this.props.tag.id}`}
+          className="edit-tag-form"
           value={this.state.title}
           onChange={(e) => this.handleChange()}
         ></input>
