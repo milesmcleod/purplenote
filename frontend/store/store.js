1331c53bc03bce0062c9bbeacc5cdeefadf0c31d
Miles McLeod <milesmcleod123@gmail.com>
2017-12-01 13:03:19 -0800

    Add necessary files to gemfile, webpack config, store, and package.json for production ready

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,15 +2,22 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
 
 
 const configureStore = (preloadedState = {}) => {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   return createStore(
     rootReducer,
     preloadedState,
-    composeEnhancers(applyMiddleware(thunk, logger))
+    applyMiddleware(...middlewares)
   );
 };
 
